fff70a0346016d1881b8859eec8a428d09e64d9b
Dan Bourne <dan311095@hotmail.co.uk>
2021-06-05 15:28:28 +0100

    fixed image sizing and replaced empty descriptions with owner name

--- src/components/PlaylistPanel.vue
+++ src/components/PlaylistPanel.vue
@@ -2,15 +2,22 @@
     <div class="playlist-container">
         <img class="playlist-image" :src="imageUrl" alt="playlist image">
         <h3 class="playlist-name"> {{ name }}</h3>
-        <p class="playlist-desc" v-html="description"></p>
+        <p class="playlist-desc" v-html="descriptionText"></p>
     </div>
 </template>
 
 <script>
+import { ref } from '@vue/reactivity';
+
 export default {
-    props: ['playlistID', 'name', 'description', 'imageUrl'],
-    setup() {
-        
+    props: ['playlistID', 'name', 'description', 'imageUrl', 'owner'],
+    setup(props) {
+        const descriptionText = ref("");
+        descriptionText.value = props.description === "" ? `by ${props.owner}` : props.description
+
+        return {
+            descriptionText
+        }
     }
 }
 </script>
