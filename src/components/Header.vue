38b37f18ac6b6a63369ddaf07f2be6f73899eee4
Dan Bourne <dan311095@hotmail.co.uk>
2021-06-06 19:40:25 +0100

    a few visual touches

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="top-bar">
-    <div class="back-forward-buttons">
-      <button><i class="fas fa-chevron-left"></i></button>
-      <button><i class="fas fa-chevron-right"></i></button>
-    </div>
-    <div v-if="currentUser" class="user-widget">
-      <img :src="currentUser.images[0].url" alt="" />
-      <div>{{ currentUser.display_name }}</div>
-    </div>
+      <div class="back-forward-buttons">
+        <button><i class="fas fa-chevron-left"></i></button>
+        <button><i class="fas fa-chevron-right"></i></button>
+      </div>
+      <div v-if="currentUser" class="user-widget">
+        <img :src="currentUser.images[0].url" alt="" />
+        <div>{{ currentUser.display_name }}</div>
+      </div>
   </div>
 </template>
 
